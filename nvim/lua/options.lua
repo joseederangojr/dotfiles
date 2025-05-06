addbed74dde531481426811b6e5fd1afb8e18def
Jose Ederango Jr <joseederangojr@gmail.com>
2025-05-06 13:11:35 +0800

    transparent bg

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -47,14 +47,14 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
+vim.opt.list = false
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+-- vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 8
